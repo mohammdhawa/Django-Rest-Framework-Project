a87f5dc88846a00292674ade329abca4d61f627f
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2024-11-21 21:56:24 +0300

    add create method to apply post method in movie_list api in order to apply add movie feature

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from watchlist_app.models import Movie
 
 
 class MovieSerializer(serializers.Serializer):
@@ -6,3 +7,17 @@ class MovieSerializer(serializers.Serializer):
     name = serializers.CharField()
     description = serializers.CharField()
     active = serializers.BooleanField()
+
+    def create(self, validated_data):
+        """
+        Create and return a new Movie instance using the validated data.
+
+        Args:
+            validated_data (dict): The data that has been validated and
+                                   is ready for creating a new Movie object.
+
+        Returns:
+            Movie: The newly created Movie instance.
+        """
+        movie = Movie.objects.create(**validated_data)
+        return movie
--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -5,12 +5,20 @@ from .serializers import MovieSerializer
 from watchlist_app.models import Movie
 
 
-@api_view() # Here by default it use GET method
+@api_view(['GET', 'POST']) # Here by default it use GET method
 def movie_list_api(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True)
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
 
-    return Response(serializer.data)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
 
 
 @api_view() # Here by default it use GET method
